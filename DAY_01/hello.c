4a0b827ce296741788aba0be3fe732922ceb30d7
ᴀsʜᴜ <ashutoshgoswami24@hotmail.com>
2024-11-22 20:44:01 +0530

    Create hello.c

---
+++ DAY_01/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // This is a header file, and it includes the standard input/output library.
+
+int main() {
+    printf("Hello, World!"); // This line prints "Hello, World!" to the console.
+    printf("\n"); // This adds a new line after the output.
+    return 0; // This indicates that the program executed successfully.
+}
